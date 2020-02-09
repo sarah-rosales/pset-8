4b420b9672bc9641b2674d90c22e07ccf4035860
Sarah Rosales <srosales@ucvts.org>
2020-02-09 16:15:25 -0500

    gotta change some functions

--- js/app.js
+++ js/app.js
@@ -1,72 +1,34 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
 
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+const winnningConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
 var board;
-let turn;
-let win;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
-const humanPlayer = "O";
-const computerPlayer = "X"
-
-const cells = document.querySelectorAll('.square'); startGame();
+const humanPlayer = 'O';
+const computerPlayer = 'X';
+const cells = document.querySelectorAll('.square');
+startGame();
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
-window.onload = init;
-
-document.getElementById("board").onclick = takeTurn;
-document.getElementById("reset-button").onclick = init;
+window.onload = startGame;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
-
-function init() {
-  board = ["", "", "", "", "", "", "", "", ""];
-  turn = "X";
-  win = null;
-
-  render();
-}
-
-function render() {
-  board.forEach(function(mark, index) {
-    squares[index].textContent = mark;
-  });
-
-  message.textContent =
-    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
-}
-
-function takeTurn(e) {
-  if (!win) {
-    let index = squares.findIndex(function(square) {
-      return square === e.target;
-    });
-
-    if (board[index] === "") {
-      board[index] = turn;
-      turn = turn === "X" ? "O" : "X";
-      win = getWinner();
-
-      render();
-    }
-  }
-}
 
 function startGame() {
 	document.querySelector(".endgame").style.display = "none";
@@ -74,86 +36,6 @@ function startGame() {
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].innerText = '';
 		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false);
+		cells[i].addEventListener('click', takeTurn, false);
 	}
-}
-
-
-function turnClick(square) {
-  if (typeof board[square.target.id] == "number") {
-    turn(square.target.id, humanPlayer)
-    if (!checkTie()) turn(bestSpot(), computerPlayer);
-  }
-}
-
-function turn(squareId, player) {
-	board[squareId] = player;
-	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player)
-	if (gameWon) gameOver(gameWon)
-}
-
-function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) =>
-		(e === player) ? a.concat(i) : a, []);
-	let gameWon = null;
-	for (let [index, win] of winCombos.entries()) {
-		if (win.every(elem => plays.indexOf(elem) > -1)) {
-			gameWon = {index: index, player: player};
-			break;
-		}
-	}
-	return gameWon;
-}
-
-function getWinner() {
-  let winner = null;
-
-  winningConditions.forEach(function(condition, index) {
-    if (
-      board[condition[0]] &&
-      board[condition[0]] === board[condition[1]] &&
-      board[condition[1]] === board[condition[2]]
-    ) {
-      winner = board[condition[0]];
-    }
-  });
-
-  return winner ? winner : board.includes("") ? null : "T";
-}
-
-function gameOver(gameWon) {
-	for (let index of winCombos[gameWon.index]) {
-		document.getElementById(index).style.backgroundColor =
-			gameWon.player == humanPlayer ? "blue" : "red";
-	}
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].removeEventListener('click', turnClick, false);
-	}
-	declareWinner(gameWon.player == humanPlayer ? "You win!" : "You lose.");
-}
-
-function declareWinner(who) {
-	document.querySelector(".endgame").style.display = "block";
-	document.querySelector(".endgame .text").innerText = who;
-}
-
-function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number');
-}
-
-function bestSpot() {
-	return emptySquares()[0];
-}
-
-function checkTie() {
-	if (emptySquares().length == 0) {
-		for (var i = 0; i < cells.length; i++) {
-			cells[i].style.backgroundColor = "green";
-			cells[i].removeEventListener('click', turnClick, false);
-		}
-		declareWinner("Tie Game!")
-		return true;
-	}
-	return false;
 }
